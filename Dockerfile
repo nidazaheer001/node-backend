98a44d67175657999c5f1726751644a3387df51e
nidazaheer001 <adiawesome0014@gmail.com>
2025-11-28 14:34:14 +0500

    updated cicd  workflow

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,18 @@
+# Use lightweight Node.js image
 FROM node:alpine3.18
+
+# Set working directory
 WORKDIR /app
+
+# Copy package.json and install dependencies
 COPY package.json ./
 RUN npm install
+
+# Copy the rest of the code
 COPY . .
+
+# Expose port 4000
 EXPOSE 4000
-CMD [ "npm", "run", "start" ]
+
+# Start the app
+CMD ["npm", "start"]
--- src/server.js
+++ src/server.js
@@ -3,15 +3,23 @@ import { connectDB } from './config/db.js';
 import { userRoute } from './routes/Users.js';
 
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
+// Connect to MongoDB
 connectDB();
+
+// Middleware
 app.use(express.json());
+
+// Test route
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
+// User routes
 app.use('/api/v1', userRoute);
-app.listen(PORT, () => {
+
+// Listen on all network interfaces for Docker
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
